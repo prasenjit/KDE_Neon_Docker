8ab6ba0efb3fb83b7b619137cbef608a238e6601
Prasenjit Kumar Shaw <shawprasenjit07@gmail.com>
2020-01-12 21:00:29 +0530

    Add script to launch kdeneon

--- temp.sh
+++ temp.sh
@@ -1,5 +1,7 @@
 xhost +si:localuser:$USER
 docker run --rm \
-    -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -e DISPLAY=$DISPLAY \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v /home/prasenjit/Docker/kdeneon/home:/home/neon \
     -ti \
-    neondev dolphin
+    neondev_v2 $1
